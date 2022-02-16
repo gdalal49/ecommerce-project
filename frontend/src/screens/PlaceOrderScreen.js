92e0f215a2f70ece59f53f82b93b51b6059b1b05
Prabhjout Singh Arora <preprabhsingh@gmail.com>
2022-02-16 22:54:56 +0530

    Designed Place Order Screen

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,39 +1,122 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { Navigate, useNavigate } from "react-router-dom";
-import CheckoutSteps from "./CheckoutSteps";
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { addToCart } from '../actions/cartActions';
+import { CART_ADD_ITEM } from '../constants/cartConstants'
+import CheckoutSteps from './CheckoutSteps'
+
 export default function PlaceOrderScreen() {
-    const cart=useSelector(state=>state.cart);
-    console.log(cart);
-    const navigate=useNavigate();
-    useEffect(()=>{
-        if(Object.keys(cart.shippingAddress).length===0){
-            navigate('/payment');
-        }
-    },[cart]);
+    const Navigate = useNavigate();
+    const dispatch = useDispatch();
+    const cart = useSelector(state => state.cart);
+    const toPrice = (num) => Number(num.toFixed(2));
+    if (!cart.paymentMethod) {
+        Navigate('/payment');
+    }
+    cart.itemsPrice = toPrice(
+    cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
+    );
+    cart.shippingPrice = cart.itemsPrice > 100 ? toPrice(0) : toPrice(10);
+    cart.taxPrice = toPrice(0.15 * cart.itemsPrice);
+    cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
+    const placeOrderHandler = () => {
+         // TODO: dispatch place order action
+    };
     return (
         <div>
-            <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
-            <div className="row top">
+            <CheckoutSteps step1 step2 step3 step4 > </CheckoutSteps>
+            <div className = "row top" >
                 <div className="col-2">
-                    <li>
-                        <ul>
-                            <div className="card card-body">
-                                <h2>Shipping</h2>
-                                <p>
-                                    <strong>
-                                        Name:
-                                    </strong>
-                                    {cart.shippingAddress.fullname}<br/>
+                    <ul>
+                        <li>
+                            <div className="card card-body" >
+                                <h2> Shipping </h2>
+                                <p><strong> Name: </strong>{cart.shippingAddress.fullName} <br></br >
+                                    <strong > Address: </strong>{cart.shippingAddress.address},{cart.shippingAddress.city}, {cart.shippingAddress.postalCode}, {cart.shippingAddress.country}
                                 </p>
                             </div>
-                        </ul>
-                    </li>
+                        </li>
+                        <li>
+        <div className = "card card-body">
+                                <h2> Payment </h2>
+                                <p>
+        <strong > Method: </strong>{cart.paymentMethod} <br></br >
+                                </p>
+                            </div >
+                        </li>
+                        <li>
+        <div className = "card card-body" >
+                                <h2 > Order Items </h2>
+                                <ul > {
+                                    cart.cartItems.map((item) => (
+                                        <li key = { item.product } >
+                <div className = "row" >
+                <div>
+                <img src = { item.image }
+                alt = { item.name }
+                className = "small" / >
+                                                </div>
+                                                <div className="min-30" >
+                                                    <Link to={`/product/${item.product}`} > {item.name} </Link> </
+                                                div >
+                                                <div> {item.qty} x $ {item.price} = $ {item.qty * item.price} </div>
+                                            </div >
+                                        </li>
+            ))
+                                }
+                                </ul>
+                            </div >
+                        </li>
+                    </ul>
                 </div>
                 <div className="col-1">
-                    
+          <div className="card card-body">
+            <ul>
+              <li>
+                <h2>Order Summary</h2>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Items</div>
+                  <div>${cart.itemsPrice.toFixed(2)}</div>
                 </div>
-            </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Shipping</div>
+                  <div>${cart.shippingPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Tax</div>
+                  <div>${cart.taxPrice.toFixed(2)}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>
+                    <strong> Order Total</strong>
+                  </div>
+                  <div>
+                    <strong>${cart.totalPrice.toFixed(2)}</strong>
+                  </div>
+                </div>
+              </li>
+                <li>
+                    <button
+                  type="button"
+                  onClick={placeOrderHandler}
+                  className="primary block"
+                  disabled={cart.cartItems.length === 0}
+                >
+                  Place Order
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
